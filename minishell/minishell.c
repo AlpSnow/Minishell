17aa2b544b9df56202c9c204093e5e9bc950953b
Leo Marck <lmarck@k1r3p4.42mulhouse.fr>
2025-03-13 19:21:53 +0100

    premiere esquisse, permet d afficher le prompt et de prendre en compte la ligne de commande puis d executer un programme avec execvp en prenant en compte les argument

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mwallis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lmarck <lmarck@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 22:00:39 by mwallis           #+#    #+#             */
-/*   Updated: 2025/03/11 22:01:20 by mwallis          ###   ########.fr       */
+/*   Updated: 2025/03/13 19:17:26 by lmarck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,45 @@
 
 int	main(void)
 {
+	char	*input_line;
+	char	**input_tab;
+	int		status;
 
-	return (0);
+	while (1)
+	{
+		input_line = readline(get_prompt()); //affiche le prompt et attends la commande de l'utilisateur
+		input_tab = ft_split(input_line, ' '); //separe les arguments dans un tableau tab[0] = executable ensuite les arguments.
+		if (fork() == 0) //cree un process enfant dans lequel on pourras executer le programme
+			execvp(input_tab[0], &input_tab[0]); //execute le programme en lui donnant les arguments(echo etc..)
+		wait(&status); //le processus parent (celui de notre minishell attend la fin du processus enfant(execution du programme en commande) pour continuer
+	}
+	return	(0);
+}
+
+char	*get_prompt() //donne un prompte avec le repertoire courant et le parents comme dans zsh() c'est pas obligatoire donc pas besoin de faire exactement comme bash
+{
+	char	*pwd;
+	int		i;
+	int		j;
+	int		l;
+	char	*prompt;
+
+	pwd = getcwd(NULL, 0); //attribue le meme resultat que pwd dans le shell
+	l = ft_strlen(pwd);
+	i = l;
+	j = 2;
+	while (j && i)
+	{
+		if (pwd[i] == '/')
+			j--;
+		if (j)
+			i--;
+		else
+			i++;
+	}
+	prompt = ft_calloc(l - i + 2, sizeof(char));
+	prompt = ft_strjoin("~", &pwd[i]);
+	prompt = ft_strjoin(prompt, GRN" coquille >$ "RESET);
+	free(pwd);
+	return (prompt);
 }
--- minishell/minishell.h
+++ minishell/minishell.h
@@ -3,17 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mwallis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: lmarck <lmarck@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 21:58:58 by mwallis           #+#    #+#             */
-/*   Updated: 2025/03/11 21:59:55 by mwallis          ###   ########.fr       */
+/*   Updated: 2025/03/13 19:13:59 by lmarck           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+//j'ai inclus tout les header dont on pourrait avoir besoin je propose qu'on laisse en commentaire tantt qu'on en a
+//pas besoin comme ca on supprimeras les inutiles a la fin.
 
+# include <stdio.h>          // printf, perror
+# include <stdlib.h>         // malloc, free, exit, getenv
+# include <unistd.h>         // read, write, close, access, fork, getcwd, chdir, unlink, execve, isatty, ttyname, ...
+//# include <fcntl.h>          // open
+# include <sys/types.h>      // fork, open
+//# include <sys/stat.h>       // open, stat, lstat, fstat
+//# include <dirent.h>         // opendir, readdir, closedir
+//# include <string.h>         // strerror
+# include <sys/wait.h>       // wait, waitpid, wait3, wait4
+//# include <signal.h>         // signal, kill, sigaction, sigemptyset, sigaddset
+//# include <sys/ioctl.h>      // ioctl
+//# include <termios.h>        // tcsetattr, tcgetattr
+//# include <curses.h>         // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+//# include <term.h>           // tgetent, tgetflag, tgetnum, tgetstr, tgoto, tputs
+# include <readline/readline.h> // readline
+# include <readline/history.h>  // add_history
+# include <stddef.h>
+# include "libft.h"
 
+# ifndef COLORS_H
+#  define COLORS_H
+
+#  define RED "\033[31m" //a mettre en debut de chaine pour lui donner une couleur
+#  define GRN "\033[32m"
+#  define YEL "\033[33m"
+#  define BLU "\033[34m"
+#  define MAG "\033[35m"
+#  define CYN "\033[36m"
+#  define WHT "\033[37m"
+
+#  define RESET "\033[0m"    //a mettre a la fin pour reset la couleur exemple: printf(GRN" coquille >$ "RESET); pour imprimer en vert
+
+# endif
+
+char	*get_prompt(void);//donne une string de ce type: "~minishell/minishell coquille >$" ou minishell est le repertoire courant
 
 #endif
